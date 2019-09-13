8a23b382a5daa3592b7dccf014b7c13ef5fac148
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2019-09-13 17:20:13 -0300

    *

--- src/views/product/form.vue
+++ src/views/product/form.vue
@@ -25,6 +25,48 @@
               <money v-model="form.price" :disabled="loading" class="el-input__inner"></money>
             </el-form-item>
           </el-col>
+          <el-col :md="12" :sm="24">
+            <el-form-item label="CFOP (Código Fiscal de Operações e de Prestações)" prop="cfop">
+              <el-select filterable v-model="form.cfop" :disabled="loading">
+                <el-option v-for="item in cfops" :key="item.id" :label="item.title" :value="item.id"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>            
+          <el-col :md="12" :sm="24">
+            <el-form-item label="Unidade de medida comercial" prop="ucom">
+              <el-select filterable v-model="form.ucom" :disabled="loading">
+                <el-option v-for="item in ucoms" :key="item.id" :label="item.title" :value="item.id"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :md="12" :sm="24">
+            <el-form-item label="CSOSN (Código de Situação da Operação do Simples Nacional)" prop="csosn">
+              <el-select filterable v-model="form.csosn" :disabled="loading">
+                <el-option v-for="item in csosns" :key="item.id" :label="item.title" :value="item.id"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :md="12" :sm="24">
+            <el-form-item label="Tabela CST do IPI" prop="ipi_ipint_cst">
+              <el-select filterable v-model="form.ipi_ipint_cst" :disabled="loading">
+                <el-option v-for="item in ipi_csts" :key="item.id" :label="item.title" :value="item.id"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :md="12" :sm="24">
+            <el-form-item label="Tabela CST do PIS" prop="pis_ipint_cst">
+              <el-select filterable v-model="form.pis_ipint_cst" :disabled="loading">
+                <el-option v-for="item in pis_cofins_csts" :key="item.id" :label="item.title" :value="item.id"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :md="12" :sm="24">
+            <el-form-item label="Tabela CST do COFINS" prop="cofins_cofinsnt_cst">
+              <el-select filterable v-model="form.cofins_cofinsnt_cst" :disabled="loading">
+                <el-option v-for="item in pis_cofins_csts" :key="item.id" :label="item.title" :value="item.id"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
           <el-col :md="24" :sm="24">
             <el-form-item>
               <router-link :to="{ name: 'product' }" class="pull-left">
@@ -55,11 +97,383 @@ export default {
     return {
       loading: false,
       companies: [],
-      categories: [],
+      categories: [],      
+      pis_cofins_csts: [
+        {
+          id: '01',
+          title: 'Operação Tributável com Alíquota Básica',
+        },
+        {
+          id: '02',
+          title: 'Operação Tributável com Alíquota por Unidade de Medida de Produto',
+        },
+        {
+          id: '03',
+          title: 'Operação Tributável com Alíquota por Unidade de Medida de Produto',
+        },
+        {
+          id: '04',
+          title: 'Operação Tributável Monofásica – Revenda a Alíquota Zero',
+        },
+        {
+          id: '05',
+          title: 'Operação Tributável por Substituição Tributária',
+        },
+        {
+          id: '06',
+          title: 'Operação Tributável a Alíquota Zero',
+        },
+        {
+          id: '07',
+          title: 'Operação Isenta da Contribuição',
+        },
+        {
+          id: '08',
+          title: 'Operação sem Incidência da Contribuição',
+        },
+        {
+          id: '09',
+          title: 'Operação com Suspensão da Contribuição',
+        },
+        {
+          id: '49',
+          title: 'Outras Operações de Saída',
+        },
+      ],
+      ipi_csts: [
+        {
+          id: '50',
+          title: 'Saída Tributada',
+        },
+        {
+          id: '51',
+          title: 'Saída Tributável com Alíquota Zero',
+        },
+        {
+          id: '52',
+          title: 'Saída Isenta',
+        },
+        {
+          id: '53',
+          title: 'Saída Não-Tributada',
+        },
+        {
+          id: '54',
+          title: 'Saída Imune',
+        },
+        {
+          id: '55',
+          title: 'Saída com Suspensão',
+        },
+        {
+          id: '99',
+          title: 'Outras Saídas',
+        },
+      ],
+      csosns: [
+        {
+          id: '101',
+          title: 'Tributada pelo Simples Nacional com permissão de crédito de ICMS',
+        },
+        {
+          id: '102',
+          title: 'Tributada pelo Simples Nacional sem permissão de crédito',
+        },
+        {
+          id: '103',
+          title: 'Isenção de ICMS no Simples Nacional na faixa de receita bruta',
+        },
+        {
+          id: '201',
+          title: 'Tributada pelo Simples Nacional com permissão de crédito e cobrança do ICMS por ST',
+        },
+        {
+          id: '202',
+          title: 'Tributada pelo Simples Nacional sem permissão de crédito e com cobrança do ICMS por ST',
+        },
+        {
+          id: '203',
+          title: 'Isenção do ICMS no Simples Nacional para faixa de receita bruta e cobrança de ICMS por ST',
+        },
+        {
+          id: '300',
+          title: 'Imune de ICMS',
+        },
+        {
+          id: '400',
+          title: 'Não tributada pelo Simples Nacional',
+        },
+        {
+          id: '500',
+          title: 'ICMS cobrado anteriormente por ST ou por antecipação',
+        },
+        {
+          id: 900,
+          title: 'Outros (operações que não se enquadram nos códigos anteriores',
+        },
+      ],
+      cfops: [
+        {
+          id: '5101',
+          title: "Venda de produção do estabelecimento"
+        }
+      ],     
+      ucoms: [
+        {
+          id: "AMPOLA",
+          title: "Ampola"
+        },
+        {
+          id: "BALDE",
+          title: "Balde"
+        },
+        {
+          id: "BANDEJ",
+          title: "Bandeja"
+        },
+        {
+          id: "BARRA",
+          title: "Barra"
+        },
+        {
+          id: "BISNAG",
+          title: "Bisnaga"
+        },
+        {
+          id: "BLOCO",
+          title: "Bloco"
+        },
+        {
+          id: "BOBINA",
+          title: "Bobina"
+        },
+        {
+          id: "BOMB",
+          title: "Bombona"
+        },
+        {
+          id: "CAPS",
+          title: "Capsula"
+        },
+        {
+          id: "CART",
+          title: "Cartela"
+        },
+        {
+          id: "CENTO",
+          title: "Cento"
+        },
+        {
+          id: "CJ",
+          title: "Conjunto"
+        },
+        {
+          id: "CM",
+          title: "Centimetro"
+        },
+        {
+          id: "CM2",
+          title: "Centimetro quadrado"
+        },
+        {
+          id: "CX",
+          title: "Caixa"
+        },
+        {
+          id: "CX2",
+          title: "Caixa com 2 unidades"
+        },
+        {
+          id: "CX3",
+          title: "Caixa com 3 unidades"
+        },
+        {
+          id: "CX5",
+          title: "Caixa com 5 unidades"
+        },
+        {
+          id: "CX10",
+          title: "Caixa com 10 unidades"
+        },
+        {
+          id: "CX15",
+          title: "Caixa com 15 unidades"
+        },
+        {
+          id: "CX20",
+          title: "Caixa com 20 unidades"
+        },
+        {
+          id: "CX25",
+          title: "Caixa com 25 unidades"
+        },
+        {
+          id: "CX50",
+          title: "Caixa com 50 unidades"
+        },
+        {
+          id: "CX100",
+          title: "Caixa com 100 unidades"
+        },
+        {
+          id: "DISP",
+          title: "Display"
+        },
+        {
+          id: "DUZIA",
+          title: "Duzia"
+        },
+        {
+          id: "EMBAL",
+          title: "Embalagem"
+        },
+        {
+          id: "FARDO",
+          title: "Fardo"
+        },
+        {
+          id: "FOLHA",
+          title: "Folha"
+        },
+        {
+          id: "FRASCO",
+          title: "Frasco"
+        },
+        {
+          id: "GALAO",
+          title: "Galão"
+        },
+        {
+          id: "GF",
+          title: "Garrafa"
+        },
+        {
+          id: "GRAMAS",
+          title: "Gramas"
+        },
+        {
+          id: "JOGO",
+          title: "Jogo"
+        },
+        {
+          id: "KG",
+          title: "Quilograma"
+        },
+        {
+          id: "KIT",
+          title: "Kit"
+        },
+        {
+          id: "LATA",
+          title: "Lata"
+        },
+        {
+          id: "LITRO",
+          title: "Litro"
+        },
+        {
+          id: "M",
+          title: "Metro"
+        },
+        {
+          id: "M2",
+          title: "Metro quadrado"
+        },
+        {
+          id: "M3",
+          title: "Metro cúbico"
+        },
+        {
+          id: "MILHEI",
+          title: "Milheiro"
+        },
+        {
+          id: "ML",
+          title: "Mililitro"
+        },
+        {
+          id: "MWH",
+          title: "Megawatt hora"
+        },
+        {
+          id: "PACOTE",
+          title: "Pacote"
+        },
+        {
+          id: "PALETE",
+          title: "Palete"
+        },
+        {
+          id: "PARES",
+          title: "Pares"
+        },
+        {
+          id: "PC",
+          title: "Peça"
+        },
+        {
+          id: "POTE",
+          title: "Pote"
+        },
+        {
+          id: "K",
+          title: "Quilate"
+        },
+        {
+          id: "RESMA",
+          title: "Resma"
+        },
+        {
+          id: "ROLO",
+          title: "Rolo"
+        },
+        {
+          id: "SACO",
+          title: "Saco"
+        },
+        {
+          id: "SACOLA",
+          title: "Sacola"
+        },
+        {
+          id: "TAMBOR",
+          title: "Tambor"
+        },
+        {
+          id: "TANQUE",
+          title: "Tanque"
+        },
+        {
+          id: "TON",
+          title: "Tonelada"
+        },
+        {
+          id: "TUBO",
+          title: "Tubo"
+        },
+        {
+          id: "UNID",
+          title: "Unidade"
+        },
+        {
+          id: "VASIL",
+          title: "Vasilhame"
+        },
+        {
+          id: "VIDRO",
+          title: "Vidro"
+        }
+      ],
       form: {
         product_category_id: null,
         title: null,
-        price: 0
+        price: 0,
+        cfop: null,
+        ucom: null,
+        csosn: null,
+        ipi_ipint_cst: null,
+        pis_ipint_cst: null,
+        cofins_cofinsnt_cst: null,
       },
       rules: {
         company_id: [
@@ -85,7 +499,43 @@ export default {
             required: true,
             message: "Campo obrigatório"
           }
-        ]
+        ],
+        cfop: [
+          {
+            required: true,
+            message: "Campo obrigatório"
+          }
+        ],
+        ucom: [
+          {
+            required: true,
+            message: "Campo obrigatório"
+          }
+        ],
+        csosn: [
+          {
+            required: true,
+            message: "Campo obrigatório"
+          }
+        ],
+        ipi_ipint_cst: [
+          {
+            required: true,
+            message: "Campo obrigatório"
+          }
+        ],
+        pis_ipint_cst: [
+          {
+            required: true,
+            message: "Campo obrigatório"
+          }
+        ],
+        cofins_cofinsnt_cst: [
+          {
+            required: true,
+            message: "Campo obrigatório"
+          }
+        ],
       }
     };
   },
